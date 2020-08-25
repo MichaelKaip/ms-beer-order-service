eb65f62c0df5766adb68303a23b3af11336af0f9
Michael Kaip <michael.kaip@student.htw-berlin.de>
2020-08-25 14:13:27 +0200

    Updates the project to new BOM.

--- target/generated-sources/annotations/mk/microservices/springframework/msbeerorderservice/web/mappers/BeerOrderLineMapperImpl_.java
+++ target/generated-sources/annotations/mk/microservices/springframework/msbeerorderservice/web/mappers/BeerOrderLineMapperImpl_.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-08-20T18:12:58+0200",
+    date = "2020-08-25T13:26:17+0200",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.7 (Oracle Corporation)"
 )
 @Component
